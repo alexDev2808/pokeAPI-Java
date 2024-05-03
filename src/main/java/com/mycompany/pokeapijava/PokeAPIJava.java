dd30d1b070d7c131a7cd3f97334a1defd5f42aa5
alexhDev2808 <colores.2808.8000@gmail.com>
2024-05-03 13:26:03 -0600

    Creando proyecto

---
+++ src/main/java/com/mycompany/pokeapijava/PokeAPIJava.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.pokeapijava;
+
+/**
+ *
+ * @author alexh2808
+ */
+public class PokeAPIJava {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
